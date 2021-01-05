2ecb7e50f73ca3852acf3f1b155e3a2a7bff25e3
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-01-05 20:21:03 +0400

    removed db command from owner cog

--- bot/cogs/owner.py
+++ bot/cogs/owner.py
@@ -6,10 +6,3 @@ class Owner(commands.Cog):
     def __init__(self, bot, theme_color):
         self.bot = bot
         self.theme_color = theme_color
-
-    @commands.command(name="db")
-    @commands.is_owner()
-    async def db(self, ctx):
-        with open("bot/data/data.db", "r") as data_file:
-            discord_file = discord.File(data_file)
-            await ctx.author.send(file=discord_file)
