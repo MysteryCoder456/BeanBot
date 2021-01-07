779b6e20269131a9f93b09ec1e90617f1d34fdab
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-01-07 09:18:49 +0400

    updated defaults and data.py

--- bot/data.py
+++ bot/data.py
@@ -8,7 +8,7 @@ class UserData:
 
     @classmethod
     def create_tables(cls):
-        cls.c.execute("""CREATE TABLE IF NOT EXISTS "users" (
+        cls.c.execute("""CREATE TABLE "users" (
             "id"	INTEGER,
             "wallet"	INTEGER DEFAULT 100,
             "bank"	INTEGER DEFAULT 0,
@@ -16,13 +16,14 @@ class UserData:
             "job_id"	INTEGER DEFAULT 0,
             "job_streak"	INTEGER DEFAULT 0,
             "worked_today"	BOOL DEFAULT 0,
+            "worked_yesterday"	BOOL DEFAULT 1,
             "inventory"	TEXT DEFAULT '[]'
         )""")
         cls.conn.commit()
 
     @classmethod
     def create_new_data(cls, user):
-        cls.c.execute("INSERT INTO users VALUES (:user_id, 100, 0, 500, 0, 0, 0, '[]')", {"user_id": user.id})
+        cls.c.execute("INSERT INTO users VALUES (:user_id, 100, 0, 500, 0, 0, 0, 1, '[]')", {"user_id": user.id})
         cls.conn.commit()
         print(f"Created data entry for {user}")
 
