230cee5a095a02351a4100c9a54eef2eeeeec9ba
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-01-20 12:07:43 +0400

    epic help command

--- bot/main.py
+++ bot/main.py
@@ -24,6 +24,18 @@ bot.add_cog(Fun(bot, THEME))
 bot.add_cog(Owner(bot, THEME))
 
 
+class HelpCommand(commands.MinimalHelpCommand):
+    async def send_pages(self):
+        destination = self.get_destination()
+        e = discord.Embed(color=THEME, description='')
+        for page in self.paginator.pages:
+            e.description += page
+        await destination.send(embed=e)
+
+
+bot.help_command = HelpCommand()
+
+
 async def update_presence():
     while True:
         server_count = len(bot.guilds)
