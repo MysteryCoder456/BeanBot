7355d6134c00e24dc69d3ab75351fcd9077e9d31
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-01-24 19:30:43 +0400

    fuck opencv all my homies use pillow

--- bot/cogs/image.py
+++ bot/cogs/image.py
@@ -1,7 +1,9 @@
 import os
+import re
 import discord
 from discord.ext import commands
-import cv2
+import PIL
+from PIL import ImageFont, ImageDraw
 
 
 class Image(commands.Cog):
@@ -9,15 +11,22 @@ class Image(commands.Cog):
         self.bot = bot
         self.theme_color = theme_color
         self.images_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "images")
+        self.fonts_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "fonts")
 
     @commands.command(name="abandon", help="Baby abandon meme", brief="Baby abandon meme")
     async def abandon(self, ctx, *, text):
-        img_path = os.path.join(self.images_dir, "abandon.jpg")
+        try:
+            img_path = os.path.join(self.images_dir, "abandon.jpg")
+            font_path = os.path.join(self.fonts_dir, "Arial.ttf")
 
-        image = cv2.imread(img_path)
-        cv2.putText(image, text, (60, 305), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+            font = ImageFont.truetype(font=font_path, size=17)
+            image = PIL.Image.open(img_path)
+            draw = ImageDraw.Draw(image)
+            draw.text((60, 280), text, fill=(0, 0, 0), font=font)
 
-        cache_filename = os.path.join(self.images_dir, "cache.jpg")
-        cv2.imwrite(cache_filename, image)
+            cache_filename = os.path.join(self.images_dir, "cache.jpg")
+            image.save(cache_filename)
 
-        await ctx.send(file=discord.File(cache_filename))
+            await ctx.send(file=discord.File(cache_filename))
+        except Exception as e:
+            print(e)
