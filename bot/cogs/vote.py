956371f8a7334caa22a9fc486fb8e005e84b357a
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-01-26 21:55:52 +0400

    added vote rewards and websocket closing fix

---
+++ bot/cogs/vote.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+
+from data import UserData
+
+
+class Vote(commands.Cog):
+    def __init__(self, bot, theme_color):
+        self.bot = bot
+        self.theme_color = theme_color
+        self.vote_reward = 100
+
+    @commands.Cog.listener()
+    async def on_dbl_vote(self, data):
+        if data["type"] == "upvote":
+            user = self.bot.get_user(int(data["user"]))
+            UserData.check_user_entry(user)
+            print(f"{user.name} has upvoted the bot on Top.gg")
+            user.send(f"Thank you for voting for me on Top.gg. You have gotten {self.vote_reward} as a gift!")
+
+            UserData.c.execute("SELECT wallet FROM users WHERE id = :user_id", {"user_id": user.id})
+            wallet = UserData.c.fetchone()[0]
+
+            UserData.c.execute(
+                "UPDATE users SET wallet = :new_amount WHERE id = :user_id",
+                {
+                    "new_amount": wallet + self.vote_reward,
+                    "user_id": user.id
+                }
+            )
+            UserData.conn.commit()
+
+    @commands.command(name="vote", help="Get links to Bot List site where you can vote for me", brief="Help me grow")
+    async def vote(self, ctx):
+        vote_embed = discord.Embed(title="Vote Bean Bot for Best Bot", color=self.theme_color)
+        vote_embed.add_field(name="Top.gg", value="[Click here to vote](https://top.gg/bot/795564109922959361)")
+
+        await ctx.send(embed=vote_embed)
--- bot/main.py
+++ bot/main.py
@@ -13,6 +13,7 @@ from cogs.jobs import Jobs
 from cogs.shop import Shop
 from cogs.fun import Fun
 from cogs.image import Image
+from cogs.vote import Vote
 from cogs.owner import Owner
 
 TOKEN = os.getenv("BEAN_TOKEN")
@@ -20,6 +21,8 @@ DBL_TOKEN = os.getenv("BEAN_DBL_TOKEN")
 DBL_AUTH = os.getenv("DBL_AUTH")
 THEME = discord.Color.green()
 PREFIX = "b."
+VOTE_REWARD = 100
+
 bot = commands.Bot(PREFIX, description="Bean Bot is a fun mini-game and economy bot.")
 dbl_client = dbl.DBLClient(bot, DBL_TOKEN, webhook_port=8000, webhook_auth=DBL_AUTH)
 presence_task = None
@@ -30,6 +33,7 @@ bot.add_cog(Jobs(bot, THEME))
 bot.add_cog(Shop(bot, THEME))
 bot.add_cog(Fun(bot, THEME))
 bot.add_cog(Image(bot, THEME))
+bot.add_cog(Vote(bot, THEME))
 bot.add_cog(Owner(bot, THEME))
 
 
@@ -91,37 +95,11 @@ async def on_command_error(ctx, exception):
 
 
 @bot.event
-async def on_dbl_vote(data):
-    print("hello")
-    print(data)
-    # UserData.check_user_entry(user)
-
-    # UserData.c.execute("SELECT wallet FROM users WHERE id = :user_id", {"user_id": user.id})
-    # wallet = UserData.c.fetchone()[0]
-
-    # UserData.c.execute(
-    #     "UPDATE users SET wallet = :new_amount WHERE id = :user_id",
-    #     {
-    #         "new_amount": wallet + self.vote_reward,
-    #         "user_id": user.id
-    #     }
-    # )
-
-
-@bot.event
 async def on_ready():
     global presence_task
     UserData.create_tables()
     presence_task = bot.loop.create_task(update_presence())
     print(f"Bot logged into {len(bot.guilds)} servers.")
-
-
-@bot.event
-async def on_disconnect():
-    print("Exiting...")
-    UserData.conn.commit()
-    UserData.c.close()
-    UserData.conn.close()
 
 
 if __name__ == "__main__":
@@ -133,7 +111,15 @@ if __name__ == "__main__":
     except SystemExit:
         print()
     finally:
+        print("Exiting...")
+
+        # Close bot connection
         running = False
         time.sleep(5)
         loop.run_until_complete(bot.logout())
         asyncio.run(dbl_client.close())
+
+        # Close Database connection
+        UserData.conn.commit()
+        UserData.c.close()
+        UserData.conn.close()
