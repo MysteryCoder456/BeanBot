74a79e02cd6e67d4e75159417aff4f1f841596a3
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-01-26 18:20:22 +0400

    fixed task pending errors

--- bot/main.py
+++ bot/main.py
@@ -22,6 +22,8 @@ THEME = discord.Color.green()
 PREFIX = "b."
 bot = commands.Bot(PREFIX, description="Bean Bot is a fun mini-game and economy bot.")
 dbl_client = dbl.DBLClient(bot, DBL_TOKEN, webhook_port=8000, webhook_auth=DBL_AUTH)
+presence_task = None
+running = True
 
 bot.add_cog(Currency(bot, THEME))
 bot.add_cog(Jobs(bot, THEME))
@@ -32,11 +34,25 @@ bot.add_cog(Owner(bot, THEME))
 
 
 async def update_presence():
-    while True:
-        server_count = len(bot.guilds)
-        activity = discord.Activity(type=discord.ActivityType.playing, name=f"with the beans | {server_count} servers | {PREFIX}help")
+    while running:
+        activity = discord.Activity(type=discord.ActivityType.playing, name="with the beans!")
         await bot.change_presence(activity=activity)
-        await asyncio.sleep(10)
+        await asyncio.sleep(5)
+
+        if not running:
+            break
+
+        server_count = len(bot.guilds)
+        activity = discord.Activity(type=discord.ActivityType.watching, name=f"{server_count} servers")
+        await bot.change_presence(activity=activity)
+        await asyncio.sleep(5)
+
+        if not running:
+            break
+
+        activity = discord.Activity(type=discord.ActivityType.watching, name=f"{PREFIX}help")
+        await bot.change_presence(activity=activity)
+        await asyncio.sleep(5)
 
 
 @bot.command(name="invite", help="Get the link to invite me to another server", brief="Invite me to another server")
@@ -94,8 +110,9 @@ async def on_dbl_vote(data):
 
 @bot.event
 async def on_ready():
+    global presence_task
     UserData.create_tables()
-    bot.loop.create_task(update_presence())
+    presence_task = bot.loop.create_task(update_presence())
     print(f"Bot logged into {len(bot.guilds)} servers.")
 
 
@@ -108,10 +125,15 @@ async def on_ready():
 
 
 if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
     try:
-        bot.run(TOKEN)
+        loop.run_until_complete(bot.start(TOKEN))
     except KeyboardInterrupt:
         print()
+    except SystemExit:
+        print()
     finally:
+        running = False
+        time.sleep(5)
+        loop.run_until_complete(bot.logout())
         asyncio.run(dbl_client.close())
-
