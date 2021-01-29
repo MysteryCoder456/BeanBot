e0867a5771fdb4dba207b5a12b0096ca8d8ce877
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-01-29 17:42:39 +0400

    hmm

--- bot/cogs/words.py
+++ bot/cogs/words.py
@@ -12,7 +12,7 @@ class Words(commands.Cog):
 
     @commands.has_guild_permissions(manage_messages=True)
     @commands.command(name="trackword", aliases=["tw"], help="Track how many times a word has been used", brief="Track a word's usage")
-    async def trackword(self, ctx, word: str):
+    async def trackword(self, ctx, *, word: str):
         Data.check_guild_entry(ctx.guild)
 
         word = word.strip()
@@ -39,7 +39,7 @@ class Words(commands.Cog):
 
     @commands.has_guild_permissions(manage_messages=True)
     @commands.command(name="untrackword", aliases=["utw"], help="Untrack a word", brief="Untrack a word")
-    async def untrackword(self, ctx, word: str):
+    async def untrackword(self, ctx, *, word: str):
         Data.check_guild_entry(ctx.guild)
 
         word = word.strip()
@@ -65,7 +65,7 @@ class Words(commands.Cog):
         await ctx.send(f"The word **{word}** is no longer being tracked!")
 
     @commands.command(name="viewwordcount", aliases=["vwc"], help="View how many times a word has been said", brief="See a word's usage")
-    async def viewwordcount(self, ctx, word: str):
+    async def viewwordcount(self, ctx, *, word: str):
         Data.check_guild_entry(ctx.guild)
 
         word = word.strip()
