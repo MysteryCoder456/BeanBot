ba21c717538c32c3d7458a6112a7dd7c00ca9c42
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-01-29 17:39:46 +0400

    added ability to untrack words

--- bot/cogs/words.py
+++ bot/cogs/words.py
@@ -37,8 +37,35 @@ class Words(commands.Cog):
 
         await ctx.send(f"The word **{word}** is now being tracked!")
 
+    @commands.has_guild_permissions(manage_messages=True)
+    @commands.command(name="untrackword", aliases=["utw"], help="Untrack a word", brief="Untrack a word")
+    async def untrackword(self, ctx, word: str):
+        Data.check_guild_entry(ctx.guild)
+
+        word = word.strip()
+
+        Data.c.execute("SELECT tracked_words FROM guilds WHERE id = :guild_id", {"guild_id": ctx.guild.id})
+        tracked_words = json.loads(Data.c.fetchone()[0])
+
+        try:
+            del tracked_words[word]
+        except KeyError:
+            await ctx.send("This word was never being tracked...")
+            return
+
+        Data.c.execute(
+            "UPDATE guilds SET tracked_words = :new_words WHERE id = :guild_id",
+            {
+                "new_words": json.dumps(tracked_words),
+                "guild_id": ctx.guild.id
+            }
+        )
+        Data.conn.commit()
+
+        await ctx.send(f"The word **{word}** is no longer being tracked!")
+
     @commands.command(name="viewwordcount", aliases=["vwc"], help="View how many times a word has been said", brief="See a word's usage")
-    async def viewwordcount(self, ctx, word: str = None):
+    async def viewwordcount(self, ctx, word: str):
         Data.check_guild_entry(ctx.guild)
 
         word = word.strip()
