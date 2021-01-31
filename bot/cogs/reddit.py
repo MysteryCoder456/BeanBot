8c6febff88214d1b2bf8e883e27859255f872501
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-01-31 22:07:02 +0400

    added nsfw filter

--- bot/cogs/reddit.py
+++ bot/cogs/reddit.py
@@ -25,20 +25,28 @@ class Reddit(commands.Cog):
         listing = None
 
         if category == "top":
-            listing = subred.top(limit=1)
+            listing = subred.top(limit=5)
         elif category == "hot":
-            listing = subred.hot(limit=1)
+            listing = subred.hot(limit=5)
         elif category == "controversial":
-            listing = subred.controversial(limit=1)
+            listing = subred.controversial(limit=5)
         elif category == "new":
-            listing = subred.new(limit=1)
+            listing = subred.new(limit=5)
         elif category == "gilded":
-            listing = subred.gilded(limit=1)
+            listing = subred.gilded(limit=5)
         else:
             await ctx.send("That category is non-existent, just like your common sense smh...")
             return
 
         post = next(listing)
+
+        if not ctx.channel.is_nsfw():
+            if subred.over18:
+                await ctx.send("This subreddit is marked NSFW. Please set this channel to NSFW to view this subreddit.")
+                return
+
+            while not post.over_18:
+                post = next(listing)
 
         post_embed = discord.Embed(title=post.title, color=self.theme_color, url=post.url)
         if post.selftext == "":
